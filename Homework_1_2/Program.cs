8b183eb51f95eb082b4e0592e6fb40d45b6dc8b9
rn4nv <dorjuan@ya.ru>
2022-12-05 06:51:55 +0700

    Completed task two

---
+++ Homework_1_2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+
+Console.Write("Введите число: ");
+var a = Console.ReadLine();
+float resultA = float.Parse(a);
+
+if (resultA % 2 == 0)
+{
+    Console.WriteLine("Введенное число четное.");
+}
+else
+{
+    Console.WriteLine("Введенное число нечетное.");
+}
