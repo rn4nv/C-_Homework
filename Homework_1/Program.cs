153c161dfaa90ccc9b6fc8f7c95d52f5bc052e2d
rn4nv <dorjuan@ya.ru>
2022-12-05 06:29:10 +0700

    Completed task one

---
+++ Homework_1/Program.cs
@@ -0,0 +1,40 @@
+﻿Console.Clear();
+
+Console.Write("Введите первое число: ");
+var a = Console.ReadLine();
+int resultA= int.Parse(a);
+Console.Write("Введите второе число: ");
+var b = Console.ReadLine();
+int resultB = int.Parse(b);
+Console.Write("Введите третье число: ");
+var c = Console.ReadLine();
+int resultC = int.Parse(c);
+
+var max = resultA;
+
+if (resultA > resultB)
+{
+   if (resultA > resultC)
+    {
+        max = resultA;
+    }
+    else
+    {
+        max = resultC;
+    }
+}
+else
+{
+    if (resultB > resultC)
+    {
+        max = resultB;
+    }
+    else
+    {
+        max = resultC;
+    }
+
+}
+
+
+Console.WriteLine(max);
