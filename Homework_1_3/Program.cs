9ee0f1b9b5040f0e0524b66c7558ca6febbff376
rn4nv <dorjuan@ya.ru>
2022-12-05 07:37:04 +0700

    Completed task three

---
+++ Homework_1_3/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.Clear();
+
+Console.Write("Введите число: ");
+string a = Console.ReadLine();
+int resultA = int.Parse(a);
+int i = 1;
+
+if (resultA > 0)
+{
+    while (i <= resultA)
+    {
+        if (i % 2 == 0)
+        {
+            Console.Write(String.Join(",", i));
+            i++;
+        }
+        else 
+        {
+            i++;
+        }
+    }
+}
+else
+{
+    int n = -1;
+    while (n >= resultA)
+        if (n % 2 == 0)
+        {
+            Console.Write(n);
+            n--;
+        }
+        else
+        {
+            {
+                n--;
+            }
+        }
+}
